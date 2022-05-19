3741c6a5048f848851fd08bf89a0caa3d63cccfb
Marcin Frasz <noahy21@gmail.com>
2022-05-19 19:26:00 +0200

    Add project files.

---
+++ DbManipulationApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DbManipulationApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
